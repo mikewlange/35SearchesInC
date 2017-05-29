23c5c4719c598b3678014657ffff1f2a0b1f0cf1
mikewlange <mikewlange@gmail.com>
2017-05-29 03:52:50 -0400

    added the copyleft for this beast of a search.

--- Motekim Search/Railgun String Search/MTKRailgun.h
+++ Motekim Search/Railgun String Search/MTKRailgun.h
@@ -5,6 +5,11 @@
 //  Created by Michael Lange on 5/28/17.
 //  Copyright © 2017 motekim. All rights reserved.
 //
+// Railgun_Trolldom (the successor of Railgun_Swampshine_BailOut - avoiding second pattern comparison in BMH2 and pseudo-BMH4), copyleft 2016-Aug-19, Kaze.
+// Railgun_Swampshine_BailOut, copyleft 2016-Aug-10, Kaze.
+// Internet "home" page: http://www.codeproject.com/Articles/250566/Fastest-strstr-like-function-in-C
+// My homepage (homeserver, often down): http://www.sanmayce.com/Railgun/
+
 
 #import <Foundation/Foundation.h>
 
